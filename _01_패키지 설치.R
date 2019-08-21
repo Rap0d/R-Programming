234ce0bf9a2a39d224ff03bc40764381fcb88fa8
rap0d <changsh9155@naver.com>
2019-08-21 17:50:03 +0900

    update R file

---
+++ _01_패키지 설치.R
@@ -0,0 +1,11 @@
+# 패키지 설치 
+# install.packages('ggplot2')
+
+# 패키지 로드 
+library(ggplot2)
+
+x <- c('a', 'a', 'b', 'c')
+
+# 그래프 출력 
+qplot(x)
+
---
+++ 기본 자료형 실습.R
@@ -0,0 +1,36 @@
+# 주석은 #으로 시작한다. 
+# <- 는 대입 연산자 
+# 문자열은 외따옴표나 쌍따옴표로 
+name <- '홍길동'
+age <- 35
+
+# 변수 이름을 입력하면 출력된다. 
+name
+age
+
+int <- 20
+string <- "김철수"
+boolean <- T # TRUE
+sum(10, 20, 30, NA) # NA : Not Available
+
+# T는 True를 의미한다. 
+sum(10, 20, 30, NA, na.rm = T)
+
+ls() # 현재 사용중인 변수 목록 보기 
+
+x <- 100
+
+is.character(string)
+is.numeric(int)
+is.logical(boolean)
+is.na(x)
+
+y <- '100'
+result <- 3 * y # ERROR
+newy <- as.numeric(y)
+result <- 3 * newy
+result
+
+# mode와 class 함수
+mode(result)
+class(result)
