384c712692849c934d25316bf467d9f9a98b6a76
rap0d <changsh9155@naver.com>
2019-10-01 12:13:09 +0900

    update file

---
+++ R_script/191001_01/chkPackage.R
@@ -0,0 +1,8 @@
+# dim : 행렬 정보 보기 
+dim(available.packages())
+
+# 앞 행 6줄 보기
+head(available.packages())
+
+# 현 사용자의 세션 정보 보기 
+sessionInfo()
--- R_script/191001_01/script02.R
+++ R_script/191001_01/script02.R
@@ -1,10 +1,3 @@
 c <- 5
 d <- 3
 result <- c * d
-
-
-dim(available.packages())
-
-head(available.packages())
-
-sessionInfo()
