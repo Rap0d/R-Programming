827f49c9e3e630b1507a85e6bd493573d7db3678
rap0d <changsh9155@naver.com>
2019-10-08 17:43:07 +0900

    update post

--- stringrEx.R
+++ stringrEx.R
@@ -36,3 +36,98 @@ unlist(m)
 # 2017-02-05 수입3000원,2017-02s-05 수입3000원,2017-02-05 수입3000원
 
 paste(data, collapse = ',')
+
+
+#########################
+
+# --------------------------
+# 다음 문자열을 정규 표현식을 이용하여 풀어 보세요.
+# 문자열에서 소괄호 안의 수치는 1개당 단가이다.
+# str1 <- '사과 3(1000)개, 밤 5(500)개, 배 4(2000)개만 주세요.'
+#
+# 출력 결과
+# 총 구입 갯수 : 12개
+# 총 판매 금액 : 13500원
+# --------------------------
+str1 <- '사과 3(1000)개, 밤 5(500)개, 배 4(2000)개만 주세요.'
+price <- c()
+qty <- c()
+res <- 0
+
+price <- c(str_extract_all(str1, '\\(\\d{3,}\\)'))
+price <- c(str_extract_all(price, '\\d{3,}'))
+price <- unlist(price)
+
+qty <- c(str_extract_all(str1, '\\d{1}\\('))
+qty <- c(str_extract_all(qty, '\\d{1}'))
+qty <- unlist(qty)
+
+res <- as.integer(price) * as.integer(qty)
+res
+
+tot <- 0
+i <- 1
+while (i <= length(res)) {
+  tot <- tot + res[i]
+  i <- i + 1
+}
+
+# 다음과 같은 주소지 정보가 있다.
+# 마지막 번지와 관련된 정보를 추출하고자 한다.
+# 
+# mydata <- c('강원원주시웅비2길8'), '강원도철원군서면와수로181번길7-16', '강원평창군봉평면태기로68', '강원강릉시강변로410번길36')
+# 
+# 출력 결과
+# 2길8
+# 181번길7-16
+# 68
+# 410번길36
+# ----------------------------
+mydata <- c('강원원주시웅비2길8', '강원도철원군서면와수로181번길7-16', '강원평창군봉평면태기로68', '강원강릉시강변로410번길36')
+v <- c()
+regex <- '\\d{1,}(\\w{1,}-?\\d{1,})?'
+for(i in mydata) {
+  ro <- str_extract_all(i, regex)
+  v <- c(v, ro)
+}
+
+# 다음 항목들 중에서 올바른 이메일 주소들만 정규 표현식을 이용하여 아이디와 메일 주소를 추출하세요.
+# 메일 주소의 id는 반드시 알파벳 소문자로 시작해야 한다.
+# id는 반드시 4글자 이상이어야 한다.
+# mydata1 <- c('abcd@naver.com', '1aaa@daum.net', 'xyz1@daum.net', 'xyz@daum.net')
+# 
+# 출력 결과
+# 아이디 : abcd, 메일 종류 : naver.com
+# 아이디 : xyz1, 메일 종류 : daum.net
+# ------------------------------
+
+mydata1 <- c('abcd@naver.com', '1aaa@daum.net', 'xyz1@daum.net', 'xyz@daum.net')
+
+vec <- c()
+
+for(i in mydata1) {
+  tmp1 <- str_extract(i, '(^[a-z]{1})(\\w{3,})')
+  tmp2 <- str_extract(i, '@([a-z]{4,}).(\\w{1,})')
+  tmp2 <- str_replace(tmp2, '@', '')
+  if(is.na(tmp1) | is.na(tmp2)) {
+    next()
+  } else {
+    vec <- c(vec, tmp1, tmp2)
+  }
+} 
+vec
+
+i <- 1
+
+# 1
+tot
+
+# 2
+v
+
+# 3
+while (i <= length(vec)) {
+  cat('id:', vec[i], ', mail : ', vec[i+1], '\n')
+  i <- i + 2
+}
+
