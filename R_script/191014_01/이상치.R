18f544c1937485932e566501604dc077a54ccd7d
Rap0d <40348849+Rap0d@users.noreply.github.com>
2019-10-14 14:45:11 +0900

    Update 이상치.R

--- R_script/191014_01/이상치.R
+++ R_script/191014_01/이상치.R
@@ -4,7 +4,7 @@
 # 이상치 : 표현하고자 하는 범주 바깥의 존재할 수 없는 값 
 
 gender <- c(1, 2, 3, 1, 2, 1, 2)
-score <- c(2, 3, 1, 5, 2, 7, 4)
+score <- c(2, 3, 1, 5, 2, 20, 4)
 
 outlier <- data.frame(gender = gender, score = score)
 outlier
@@ -29,4 +29,17 @@ outlier
 
 # useNA = 'ifany' : NA 보기
 table(outlier$score, useNA = 'ifany')
+
+library(dplyr)
+
+# NA값 필터링 후 평균값 구하기 
+outlier %>% filter(!is.na(gender) & !is.na(score)) %>% group_by(gender) %>% summarise(mean_score = mean(score))
+# # A tibble: 2 x 2
+# gender  mean_score
+# <dbl>      <dbl>
+#     1        3.5
+#     2        3  
+
+# boxplot : 데이터의 분포를 볼 수 있음
+boxplot(score)
 
