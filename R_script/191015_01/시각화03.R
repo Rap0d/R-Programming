e895cdab7e5d16885de793dac678f0904e5a1f8c
Rap0d <40348849+Rap0d@users.noreply.github.com>
2019-10-15 12:44:01 +0900

    Update 시각화03.R

--- R_script/191015_01/시각화03.R
+++ R_script/191015_01/시각화03.R
@@ -32,4 +32,21 @@ barplot(c1, beside = T, main = '과목별 학생 점수', col = mycolor, ylim = 
 # t() 함수를 사용한 행렬 전치
 # 학생별 과목 점수
 c2 <- t(c1)
+
+# 학생 이름도 t()함수를 통해 전치하여 넣음
+colnames(c2) <- t(name)
 barplot(c2, beside = T, main = '학생별 과목 점수', col = mycolor, ylim = c(0, 100))
+
+namesubject
+
+# 1번째 학생만 그려보기
+c3 <- c1[1,]
+c3
+subject <- colnames(namesubject[2:7])
+subject
+
+# legend : 범례 
+barplot(c3, beside = T, main = '개인의 과목 점수', col = mycolor, ylim = c(0, 100), legend = subject)
+
+
+pie(c3, main = '개인의 과목 점수 파이 그래프')
