33ff0b5a97447f01226d5b03459a31cd900b4877
Rap0d <40348849+Rap0d@users.noreply.github.com>
2019-10-23 11:55:20 +0900

    Update savfileEx.R

--- R_script/largedata/savfileEx.R
+++ R_script/largedata/savfileEx.R
@@ -14,3 +14,15 @@ str(raw_data)
 welfare <- raw_data[c('h10_g3', 'h10_g4', 'h10_g10', 'h10_g11', 'h10_eco9', 'p1002_8aq1', 'h10_reg7')]
 str(welfare)
 dim(welfare)
+
+# help(rename)
+# 가독성 있게 컬럼 이름을 변경( colnames로도 가능)
+welfare <- rename(welfare, gender=h10_g3, birth=h10_g4, 
+                  marriage=h10_g10, religion=h10_g11, code_job=h10_eco9, 
+                  income=p1002_8aq1, code_region=h10_reg7 )
+head(welfare)
+tail(welfare)
+dim(welfare) # 16664     7
+str(welfare)
+
+write.csv(welfare, 'welfare.csv', row.names=F, quote=F)
