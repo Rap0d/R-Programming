4aae8fc57577d3ac44a5f4f2b86338aae79ef4a4
Rap0d <40348849+Rap0d@users.noreply.github.com>
2019-11-15 12:38:30 +0900

    update file

--- R_script/bayes/bayes05.R
+++ R_script/bayes/bayes05.R
@@ -1,22 +1,12 @@
 library(e1071)
 library(ggplot2)
 
-setwd('R Basic Source/32.Naive Bayes/')
-data <- read.csv('personinfo.csv')
+data <- read.csv('mushrooms.csv')
 head(data)
 str(data)
 summary(data)
 dim(data)
 colnames(data)
-
-range(data$age)
-data$age2[data$age >= 10 & data$age < 20] <- '10대'
-data$age2[data$age >= 20 & data$age < 30] <- '20대'
-data$age2[data$age >= 30 & data$age < 40] <- '30대'
-data$age2[data$age >= 40 & data$age < 50] <- '40대'
-
-data$age <- NULL
-data
 
 idx <- sample(1:nrow(data), 0.7 * nrow(data))
 training <- data[idx,]
@@ -25,7 +15,7 @@ testing <- data[-idx,]
 model <- naiveBayes(type ~ ., data = training, laplace = 0)
 model
 
-pred <- predict(model, testing[-5])
+pred <- predict(model, testing[-1])
 pred
 
 table(예측 = pred, 정답 = testing$type)
